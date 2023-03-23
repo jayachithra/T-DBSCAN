c971e4f013ca535d1e49799f58e86be97bce514c
Jaya <kumarjaya@microsoft.com>
2023-03-24 00:17:52 +0100

    Added setup and license; restructured code

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'T-DBSCAN algorithm'
+LONG_DESCRIPTION = 'A package that clusters data using the T-DBSCAN algorithm'
+
+# Setting up
+setup(
+    name="vidstream",
+    version=VERSION,
+    author="Jayachithra",
+    author_email="<s.k.jayachithra@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['geopy'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ tdbscan/__init__.py
@@ -0,0 +1,1 @@
+from tbscan import TDBSCAN
